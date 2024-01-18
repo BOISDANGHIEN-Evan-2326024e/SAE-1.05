3aaa3bf76d6c6f1cff09f823125be2e45d0d4cfb
DOUCHY-Maxime-2326038u <146344643+DOUCHY-Maxime-2326038u@users.noreply.github.com>
2024-01-18 10:53:56 +0100

    Add files via upload

---
+++ site_departement_info/javascript/formulaire.js
@@ -0,0 +1,181 @@
+
+if (localStorage.getItem('co') == 1)
+{
+    document.getElementsByClassName("message")[0].textContent = "Vous êtes connecté en tant que " + String(JSON.parse(localStorage.getItem('un'))[localStorage.getItem('user')]);
+    document.getElementById("submit").style.display = "none"
+    document.getElementById("mdp").style.display = "block"
+    document.getElementById("deco").style.display = "block"
+    document.getElementsByClassName("message")[0].style.color = "#53af57";
+    document.getElementById("cache").style.display = "none";
+    if (JSON.parse(localStorage.getItem('un'))[localStorage.getItem('user')] == "admin")
+    {
+        document.getElementById("reset").style.display = "block"
+    }
+}
+
+if (localStorage.getItem('reset') == 1)
+{
+    document.getElementsByClassName("message")[0].textContent = "Les mots de passes ont été réinitialisé avec succès";
+    document.getElementsByClassName("message")[0].style.color = "#53af57";
+    localStorage.setItem('reset', 0)
+}
+
+var nom = "";
+var pass = "";
+var pass2 = "";
+
+function envoie()
+{
+    if (!changement)
+    {
+        nom = document.getElementById("username").value
+        pass = document.getElementById("password").value
+    
+        for (var i in JSON.parse(localStorage.getItem('un')))
+        {
+            if (nom == JSON.parse(localStorage.getItem('un'))[i])
+            {
+                if (pass == JSON.parse(localStorage.getItem('pw'))[i])
+                {
+                    localStorage.setItem('co', 1);
+                    localStorage.setItem('user', i);
+                    location.reload();
+                }
+                else
+                {
+                    document.getElementsByClassName("message")[0].textContent = "Mot de passe invalide.";
+                    document.getElementsByClassName("message")[0].style.color = "#af5353";
+                }
+                return;
+            }
+        }
+        document.getElementsByClassName("message")[0].textContent = "Nom d'utilisateur invalide."
+        document.getElementsByClassName("message")[0].style.color = "#af5353";
+    }
+    else
+    {
+        nom = document.getElementById("username").value;
+        pass = document.getElementById("password").value;
+        pass2 = document.getElementById("cpassword").value;
+        if (nom == JSON.parse(localStorage.getItem('pw'))[localStorage.getItem('user')])
+        {
+            if (pass.length > 6)
+            {
+                if (pass == pass2)
+                {
+                    document.getElementsByClassName("message")[0].textContent = "Mot de passe changé avec succès."
+                    document.getElementsByClassName("message")[0].style.color = "#53af57";
+                    var nouvelleListe = JSON.parse(localStorage.getItem('pw'));
+                    nouvelleListe[localStorage.getItem('user')] = pass;
+                    localStorage.setItem('pw', JSON.stringify(nouvelleListe));
+                    cacherForm();
+                    modif();
+                    return;
+                }
+                document.getElementsByClassName("message")[0].textContent = "Les mots de passes de correspondent pas."
+                document.getElementsByClassName("message")[0].style.color = "#af5353";
+                return;
+            }
+            document.getElementsByClassName("message")[0].textContent = "Le mot de passe doit contenir au moins 7 caractères."
+            document.getElementsByClassName("message")[0].style.color = "#af5353";
+            return;
+        }
+        document.getElementsByClassName("message")[0].textContent = "Ancien mot de passe incorrect."
+        document.getElementsByClassName("message")[0].style.color = "#af5353";
+        return;
+    }
+}
+var changement = false
+function modif()
+{
+    if (!changement)
+    {
+        document.getElementById("deco").style.display = "none";
+        document.getElementById("submit").style.display = "block";
+        document.getElementById("submit").value = "Confirmer";
+        document.getElementById("mdp").value = "annuler";
+        nom = document.getElementById("username").value = "";
+        pass = document.getElementById("password").value = "";
+        pass2 = document.getElementById("cpassword").value = "";
+        montrerForm();
+        changement = true;
+        document.getElementsByClassName("message")[0].textContent = "Vous êtes connecté en tant que " + String(JSON.parse(localStorage.getItem('un'))[localStorage.getItem('user')]);
+        document.getElementsByClassName("message")[0].style.color = "#53af57";
+    }
+    else
+    {
+        document.getElementById("submit").style.display = "none";
+        document.getElementById("deco").style.display = "block"
+        document.getElementById("mdp").value = "Modifier mon mot de passe"
+        cacherForm();
+        changement = false;
+    }
+}
+
+function deco()
+{
+    localStorage.setItem('co', 0);
+    document.getElementById("deco").style.display = "none"
+    location.reload();
+}
+
+function apresReset()
+{
+    var un = JSON.stringify(['Maxime', 'Edson', 'Evan', 'Vincent', 'admin'])
+    var pw = JSON.stringify(['azerty', 'cactusmucche', 'legoat', 'prout', 'admin'])
+
+    localStorage.setItem('un', un);
+    localStorage.setItem('pw', pw);
+    console.log(typeof localStorage.getItem('un'))
+
+    console.log(JSON.parse(localStorage.getItem('un')));
+    console.log(JSON.parse(localStorage.getItem('pw')));
+    localStorage.setItem('reset', 1);
+    location.reload();
+}
+
+function reset()
+{
+    localStorage.clear();
+    apresReset();
+    
+}
+
+
+function montrerForm()
+{
+    document.getElementById("cache").style.display = "block";
+    document.getElementById("cache2").style.display = "block";
+    document.getElementById("inf1").textContent = "Ancien mot de passe";
+    document.getElementById("username").type = "password";
+    document.getElementById("username").placeholder = "Entrez votre ancien mot de passe";
+    document.getElementById("inf2").textContent = "Nouveau mot de passe";
+    document.getElementById("password").placeholder = "Entrez votre nouveau mot de passe";
+    document.getElementById("inf3").textContent = "Confirmez le nouveau mot de passe";
+    document.getElementById("password").placeholder = "Entrez de nouveau votre nouveau mot de passe";
+    document.getElementById("tite").textContent = "Modification";
+    document.getElementById("reset").style.display = "none";
+}
+function cacherForm()
+{
+    document.getElementById("cache").style.display = "none";
+    document.getElementById("cache2").style.display = "none";
+    console.log(localStorage.getItem('user'))
+    if (String(JSON.parse(localStorage.getItem('un'))[localStorage.getItem('user')]) == "admin")
+    {
+        document.getElementById("reset").style.display = "block";
+    }
+}
+
+if (typeof localStorage.getItem('un') == "object")
+{
+    var un = JSON.stringify(['Maxime', 'Edson', 'Evan', 'Vincent', 'admin'])
+    var pw = JSON.stringify(['azerty', 'cactusmucche', 'legoat', 'prout', 'admin'])
+
+    localStorage.setItem('un', un);
+    localStorage.setItem('pw', pw);
+    console.log(typeof localStorage.getItem('un'))
+}
+
+console.log(JSON.parse(localStorage.getItem('un')));
+console.log(JSON.parse(localStorage.getItem('pw')));
---
+++ site_departement_info/javascript/script.js
@@ -0,0 +1,76 @@
+const membre = document.getElementById("intra");
+
+membre.addEventListener("mouseenter", (e) => {
+    membre.src = "../images/logo/logo-intranet-blanc.png";
+});
+
+membre.addEventListener("mouseleave", (e) => {
+    membre.src = "../images/logo/logo-intranet.png";
+});
+
+const img1 = document.getElementsByClassName("actu")[0];
+const img2 = document.getElementsByClassName("actu")[1];
+const img3 = document.getElementsByClassName("actu")[2];
+img1.style.display = "flex";
+img2.style.display = "none";
+img3.style.display = "none";
+var img = 1;
+console.log(document.URL.substr(-10) == "index.html")
+
+function prec()
+{
+    if (img == 1)
+    {
+        img1.style.display = "none";
+        img3.style.display = "flex";
+        img = 3;
+    }
+    else if (img == 3)
+    {
+        img3.style.display = "none";
+        img2.style.display = "flex";
+        img = 2;
+    }
+    else if (img == 2)
+    {
+        img2.style.display = "none";
+        img1.style.display = "flex";
+        img = 1;
+    }
+}
+function suiv()
+{
+    if (img == 1)
+    {
+        img1.style.display = "none";
+        img2.style.display = "flex";
+        img = 2;
+    }
+    else if (img == 3)
+    {
+        img3.style.display = "none";
+        img1.style.display = "flex";
+        img = 1;
+    }
+    else if (img == 2)
+    {
+        img2.style.display = "none";
+        img3.style.display = "flex";
+        img = 3;
+    }
+}
+var carte = 0;
+function montreCarte()
+{
+    if (carte == 0)
+    {
+        document.getElementsByClassName("carte")[0].style.display = "flex";
+        carte = 1;
+    }
+    else
+    {
+        document.getElementsByClassName("carte")[0].style.display = "none";
+        carte = 0;
+    }
+    
+}
